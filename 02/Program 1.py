66a23f8c58b07fdef835b0d0e549791430f92ac9
helenawangqian <55547289+helenawangqian@users.noreply.github.com>
2019-09-28 00:37:17 +0800

    Add files via upload

---
+++ 02/Program 1.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+nam = input ('Who are you?')
+print('Hello,',nam,'!')
+
+
+# In[ ]:
+
+
+
+
---
+++ 02/Program 2.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+str_input = input ('What is the degree in Celsius?')
+C = float (str_input)
+print('The corresponding degree in Fahrenheit is:', C*1.8+32)
+
+
+# In[ ]:
+
+
+
+
